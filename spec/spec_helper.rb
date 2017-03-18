c53a2f90bcebdcacf5b8e7f66347d75fcff2250f
Arturo Guzman <arturo@guzart.com>
2017-03-18 19:21:52 -0400

    chore: add code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'awesome_print'
 require 'opie'
 require 'rspec'
