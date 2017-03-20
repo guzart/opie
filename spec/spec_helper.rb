8a4b5d9ed44555a45a2001056c8b7b75230b94be
Arturo Guzman <arturo@guzart.com>
2017-03-19 20:18:55 -0400

    chore(specs): add support for focus filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,9 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.order = :random
   config.example_status_persistence_file_path = 'tmp/spec_examples.txt'
 
